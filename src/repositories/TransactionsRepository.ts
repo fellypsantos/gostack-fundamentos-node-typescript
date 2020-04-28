0401e50b42855a7d27b1318631b985ce7cf5fe00
Fellyp Santos <fellypsantos2011@gmail.com>
2020-04-28 15:30:06 -0300

    Challenge finished.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface TransactionWithBalance {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +24,45 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): TransactionWithBalance {
+    const transactionsWithBalance: TransactionWithBalance = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+
+    return transactionsWithBalance;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncome = this.transactions
+      .filter(t => t.type === 'income')
+      .reduce(
+        (accumulator, currentValue) => (accumulator += currentValue.value),
+        0,
+      );
+
+    const totalOutcome = this.transactions
+      .filter(t => t.type === 'outcome')
+      .reduce(
+        (accumulator, currentValue) => (accumulator += currentValue.value),
+        0,
+      );
+
+    const balance: Balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalIncome - totalOutcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
